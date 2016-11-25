bdb269bcde84e6e560c5e6d0d2d063c9ef10ef71
AvijitGhosh82 <avijitg22@gmail.com>
2016-11-25 19:53:05 +0530

    Added tiled layout (WIP)

--- app.py
+++ app.py
@@ -303,11 +303,12 @@ def projects():
 	 #                  Rating=row[3]) for row in cursor.fetchall()]
 			projectsData=list()
 			for index,row in enumerate(cursor.fetchall()) :
-				projectsData.append(dict(projectLink=row[4],
-										projectName=row[5],
-										projectDescription=row[6],
-										projectHandle=row[0],
-										id=index))
+				if len(row[0])>0:
+					projectsData.append(dict(projectLink=row[4],
+											projectName=row[5],
+											projectDescription=row[6],
+											projectHandle=row[0],
+											id=index))
 			projectsData = sorted(projectsData, key=itemgetter('projectName')) 
 			return render_template('projects.html' , projectsData=projectsData)
 	except:
@@ -338,4 +339,4 @@ app.secret_key = 'kwoc'
 app.config['SESSION_TYPE'] = 'filesystem'
 
 sess.init_app(app)
-app.debug = False
+app.debug = True
