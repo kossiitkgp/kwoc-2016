9354aca5dbfa46bb67dde369480086f3c02ba38a
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2016-12-01 10:00:08 +0530

    Fixed unicode error

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 from flask import Flask, render_template, url_for, request, session, redirect
 from flask_session import Session
 import os
